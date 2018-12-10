02138417f9813660c6d04f738cdbfdcee7330baf
Bielczy <44232416+Bielczy@users.noreply.github.com>
2018-12-10 15:44:01 +0100

    Add files via upload
    ...według nowej organizacji layoutu.

--- MainActivity.java
+++ MainActivity.java
@@ -3,11 +3,10 @@ package com.example.bielczy.vmc_charts_java;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.example.bielczy.vmc_charts_java.db.CurrentDataStreamFragment;
+import com.example.bielczy.vmc_charts_java.db.TemperatureFragment;
 import com.example.bielczy.vmc_charts_java.db.ExtractByDateFragment;
 import com.example.bielczy.vmc_charts_java.db.StartFragment;
 
@@ -25,38 +24,16 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-        switch (id){
-            case R.id.current_data_stream:
-                onCurrentTemperatureClick();
-                return true;
-            case R.id.extract_by_date:
-                onExtractTemperatureClick();
-                return true;
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void onCurrentTemperatureClick() {
-
+    public void onTemperatureClick(View view) {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.action_container, new CurrentDataStreamFragment())
-                        .commit();
+                .replace(R.id.action_container, new TemperatureFragment())
+                .commit();
     }
-    private void onExtractTemperatureClick() {
 
+
+    public void onRangeClick(View view) {
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.action_container, new ExtractByDateFragment())
+                .replace(R.id.LineChartTemperature, new ExtractByDateFragment())
                 .commit();
     }
 }
